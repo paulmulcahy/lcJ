18f6d682ec4c9d00c17e91ade6e0dd48e9ea40f2
paulmulcahy <paulwmulcahy@gmail.com>
2023-12-09 13:13:49 -0800

    Leetcode Q1 Two Sum

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # leetcodeJava
 leetcode problems solved in java
+
+How to Run
+
+1. Install Java JDK (21 or later) and add 'java' to your environment variables.
+2. Install Maven and add 'mvn' to your environment variables.
+3. From leetcodeJava directory (directory that contains pom.xml), run "mvn test" in terminal/powershell to test leetcodeJava implementations.
---
+++ src/test/java/ca/pmulcahy/lc/q001/TwoSumTest.java
@@ -0,0 +1,38 @@
+package ca.pmulcahy.lc.q001;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TwoSumTest {
+
+  @Test
+  public void example1Test() {
+    int[] inputNums = {2, 7, 11, 15};
+    int inputTarget = 9;
+
+    int[] expected = {0, 1};
+    int[] actual = TwoSum.twoSum(inputNums, inputTarget);
+    Assertions.assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  public void example2Test() {
+    int[] inputNums = {3, 2, 4};
+    int inputTarget = 6;
+
+    int[] expected = {1, 2};
+    int[] actual = TwoSum.twoSum(inputNums, inputTarget);
+    Assertions.assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  public void example3Test() {
+    int[] inputNums = {3, 3};
+    int inputTarget = 6;
+
+    int[] expected = {0, 1};
+    int[] actual = TwoSum.twoSum(inputNums, inputTarget);
+    Assertions.assertArrayEquals(expected, actual);
+  }
+}
