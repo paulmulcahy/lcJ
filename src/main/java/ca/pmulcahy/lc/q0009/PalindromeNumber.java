5ec65373e1e514e18ea05d81b5dd4cc71ad6fcd3
paulmulcahy <paulwmulcahy@gmail.com>
2023-12-10 10:30:24 -0800

    9. Palindrome Number

---
+++ src/main/java/ca/pmulcahy/lc/q0009/PalindromeNumber.java
@@ -0,0 +1,18 @@
+package ca.pmulcahy.lc.q0009;
+
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+public class PalindromeNumber {
+	
+	//TODO: do it without strings
+    public static boolean isPalindrome(int x) {
+    	String num = Integer.toString(x);
+    	
+    	for(int i = 0; i < num.length() /2; i++) {
+    		if(num.charAt(i) != num.charAt(num.length() - 1 - i)) {
+    			return false;
+    		}
+    	}
+        
+    	return true;
+    }
+}
---
+++ src/test/java/ca/pmulcahy/lc/q0009/PalindromeNumberTest.java
@@ -0,0 +1,37 @@
+package ca.pmulcahy.lc.q0009;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import ca.pmulcahy.lc.q0008.StringToInteger;
+
+public class PalindromeNumberTest {
+
+	  @Test
+	  public void example1Test() {
+	    final int input = 121;
+	    final boolean actual = PalindromeNumber.isPalindrome(input);
+	    Assertions.assertTrue(actual);
+	  }
+	  
+	  @Test
+	  public void example2Test() {
+	    final int input = -121;
+	    final boolean actual = PalindromeNumber.isPalindrome(input);
+	    Assertions.assertFalse(actual);
+	  }
+	  
+	  @Test
+	  public void example3Test() {
+	    final int input = 10;
+	    final boolean actual = PalindromeNumber.isPalindrome(input);
+	    Assertions.assertFalse(actual);
+	  }
+	  
+	  @Test
+	  public void my1Test() {
+	    final int input = 2112;
+	    final boolean actual = PalindromeNumber.isPalindrome(input);
+	    Assertions.assertTrue(actual);
+	  }
+}
