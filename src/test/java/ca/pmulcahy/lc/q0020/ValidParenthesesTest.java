42988ca63fa758723348c67bade53bfdfcc47888
paulmulcahy <paulwmulcahy@gmail.com>
2023-12-16 22:00:28 -0800

    20. Valid Parentheses

---
+++ src/test/java/ca/pmulcahy/lc/q0020/ValidParenthesesTest.java
@@ -0,0 +1,29 @@
+package ca.pmulcahy.lc.q0020;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ValidParenthesesTest {
+
+  @Test
+  public void example1Test() {
+    final String input = "()";
+    final boolean actual = ValidParentheses.isValid(input);
+    Assertions.assertTrue(actual);
+  }
+
+  @Test
+  public void example2Test() {
+    final String input = "()[]{}";
+    final boolean actual = ValidParentheses.isValid(input);
+    Assertions.assertTrue(actual);
+  }
+
+  @Test
+  public void example3Test() {
+    final String input = "(]";
+    final boolean actual = ValidParentheses.isValid(input);
+    Assertions.assertFalse(actual);
+  }
+}
