c83a3f698d5f6cef8e1806a7018a26518a7720c9
paulmulcahy <paulwmulcahy@gmail.com>
2023-12-24 15:23:47 -0800

    29. Divide Two Integers

---
+++ src/test/java/ca/pmulcahy/lc/q0029/DivideTwoIntegersTest.java
@@ -0,0 +1,61 @@
+package ca.pmulcahy.lc.q0029;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DivideTwoIntegersTest {
+
+  @Test
+  public void example1Test() {
+    int inputDividend = 10;
+    int inputDivisor = 3;
+    int expected = 3;
+    int actual = DivideTwoIntegers.divide(inputDividend, inputDivisor);
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void example2Test() {
+    int inputDividend = 7;
+    int inputDivisor = -3;
+    int expected = -2;
+    int actual = DivideTwoIntegers.divide(inputDividend, inputDivisor);
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void lc1Test() {
+    int inputDividend = 1;
+    int inputDivisor = 1;
+    int expected = 1;
+    int actual = DivideTwoIntegers.divide(inputDividend, inputDivisor);
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void lc2Test() {
+    int inputDividend = -2147483648;
+    int inputDivisor = -1;
+    int expected = 2147483647;
+    int actual = DivideTwoIntegers.divide(inputDividend, inputDivisor);
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void lc3Test() {
+    int inputDividend = -2147483648;
+    int inputDivisor = 1;
+    int expected = -2147483648;
+    int actual = DivideTwoIntegers.divide(inputDividend, inputDivisor);
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void lc4Test() {
+    int inputDividend = -2147483648;
+    int inputDivisor = 2;
+    int expected = -1073741824;
+    int actual = DivideTwoIntegers.divide(inputDividend, inputDivisor);
+    Assertions.assertEquals(expected, actual);
+  }
+}
